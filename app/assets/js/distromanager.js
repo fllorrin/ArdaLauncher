c8a9d93e23a86300328bd2b2a33adc507b3fd586
BuildTools <unconfigured@null.spigotmc.org>
2021-01-06 20:08:38 +0100

    Update distromanager.js

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -537,7 +537,7 @@ exports.pullRemote = function(){
         return exports.pullLocal()
     }
     return new Promise((resolve, reject) => {
-        const distroURL = 'https://fllorrin.w-h.site/httpdocs/distribution.json'
+        const distroURL = 'http://fllorrin.w-h.site/httpdocs/distribution.json'
         //const distroURL = 'https://gist.githubusercontent.com/dscalzi/53b1ba7a11d26a5c353f9d5ae484b71b/raw/'
         const opts = {
             url: distroURL,
